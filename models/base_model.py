f94c1be3424c9bd0976c3f05e43043d2a0715d6e
Gig2341 <danmata2341@gmail.com>
2023-07-13 00:08:24 +0000

    task 4 updated

--- models/base_model.py
+++ models/base_model.py
@@ -22,9 +22,6 @@ class BaseModel():
             time_format = '%Y-%m-%dT%H:%M:%S.%f'
             self.created_at = datetime.strptime(self.created_at, time_format)
             self.updated_at = datetime.strptime(self.updated_at, time_format)
-            ''' for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, time_format)'''
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
