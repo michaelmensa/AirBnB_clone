95b0d682fc30c5ca28a4d09b98fc556f13239ec3
michaelmensa <michaelmensa@live.com>
2023-07-13 22:46:47 +0000

    task 9 update

---
+++ models/place.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+'''
+Module represents the class Place
+'''
+
+
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    '''
+    This class inherits from basemodel
+    '''
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
