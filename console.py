63afde72ef69afe580cd7cc8dc5f8f8e5d45241c
michaelmensa <michaelmensa@live.com>
2023-07-15 23:13:51 +0000

    console.py first commit

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+'''
+Module for Console
+'''
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    ''' this class represents the Console
+    '''
+
+    prompt = ' (hbnb) '
+
+    def do_EOF(self, line):
+        ''' method for EOF'''
+        return True
+
+    def do_quit(self, line):
+        ''' method to quit console'''
+        return True
+
+    def do_emptyline(self):
+        ''' empty line method '''
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
