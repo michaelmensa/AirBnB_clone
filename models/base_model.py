23ea3ad8735c07ec77705cadd498aa031bfcf231
Gig2341 <danmata2341@gmail.com>
2023-07-16 17:53:25 +0000

    fixed base model bug

--- models/base_model.py
+++ models/base_model.py
@@ -27,8 +27,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
-        models.storage.new(self)
+	    models.storage.new(self)
 
     def __str__(self):
         '''this method defines the string representation of the basemodel'''
