42d76a8915ae80ca80a8a501b154d153aff1dfcf
michaelmensa <michaelmensa@live.com>
2023-07-16 23:32:56 +0000

    task 3 bug fixed again

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-	    models.storage.new(self)
+            #models.storage.new(self)
 
     def __str__(self):
         '''this method defines the string representation of the basemodel'''
@@ -31,7 +31,8 @@ class BaseModel:
     def save(self):
         '''this method updates updated_at with current datetime'''
         self.updated_at = datetime.now()
-        models.storage.save()
+        #models.storage.new(self)
+        #models.storage.save()
 
     def to_dict(self):
         '''this method returns a dictionary containing all keys/values'''
