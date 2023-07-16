e94e08a30023a806329cef509a5983dcbc26e822
Gig2341 <danmata2341@gmail.com>
2023-07-16 22:39:29 +0000

    task 5 base model fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,17 +5,18 @@
 Module for file storage
 '''
 
-
+import models
+from models.base_model import BaseModel
 import json
 
 
-class FileStorage():
+class FileStorage:
     '''
     This class serializes instances to a JSON file and deserializes
     JSON file to instances
     '''
 
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
@@ -28,7 +29,7 @@ class FileStorage():
         ''' this public instance method sets in __objects the obj with key
         <obj class name>.id
         '''
-        self.__objects["{}.{}".format(obj.__class__.__name__.id, obj.id)] = obj
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         ''' this public instance method serializes __objects to a JSON
@@ -42,11 +43,15 @@ class FileStorage():
                 json.dump(new_dict, f)
 
     def reload(self):
-        '''this public instance method deserializes the JSON file to __objects
+        '''
+	this public instance method deserializes the JSON file to __objects
         '''
 
         try:
-            with open(self.__file_path, 'w+', encoding='utf-8') as f:
-                self.__objects = json.load(f)
-        except FileNotFoundError:
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                new_objects = json.load(f)
+                for key, value in new_objects.items():
+                    reloaded_obj = eval('{}(**value)'.format(value['__class__']))
+                    self.__objects[key] = reloaded_obj
+        except (IOError):
             pass
