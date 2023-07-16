aa08ce28218de5cc2e8ffdc47bec87c3630d90ef
Gig2341 <danmata2341@gmail.com>
2023-07-16 18:33:32 +0000

    fixed base model bug

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,9 @@ import models
 class BaseModel:
     '''this represents the basemodel class'''
 
-    '''def __init__(self, id=None, created_at=None, updated_at=None):
-        this method initializes the basemodel class
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()'''
-
     def __init__(self, *args, **kwargs):
         '''this constructor is an updated version'''
+
         if len(kwargs) != 0:
             self.__dict__ = kwargs
             time_format = '%Y-%m-%dT%H:%M:%S.%f'
