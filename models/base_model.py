5a3c80de726ad1953154a32925a39ca968b8b11d
michaelmensa <michaelmensa@live.com>
2023-07-19 00:29:16 +0000

    bug fixed pycodestyle basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,8 @@ class BaseModel():
 
     def __str__(self):
         '''this method defines the string representation of the basemodel'''
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
         '''this method updates updated_at with current datetime'''
