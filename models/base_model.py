d1908dad4564ceacbd5e223f65a150798b9edb55
michaelmensa <113622103+michaelmensa@users.noreply.github.com>
2023-07-19 00:07:57 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # models.storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         '''this method defines the string representation of the basemodel'''
@@ -32,7 +32,7 @@ class BaseModel():
     def save(self):
         '''this method updates updated_at with current datetime'''
         self.updated_at = datetime.now()
-        # models.storage.save()
+        models.storage.save()
 
     def to_dict(self):
         '''this method returns a dictionary containing all keys/values'''
